0d26052afebdaa4f48b127e458c885f09935513f
Vinicius Almeida Nunes <vinunes.2004@gmail.com>
2023-05-08 22:14:25 -0300

    committeste

--- Project1/src/main/java/com/Controller/Servlet.java
+++ Project1/src/main/java/com/Controller/Servlet.java
@@ -19,13 +19,13 @@ public class Servlet extends HttpServlet {
     }
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Ola Thiago");
+		System.out.println("");
 		
 	}
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		System.out.println("");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
