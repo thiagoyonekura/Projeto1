0c0dc26c24100a2842cc07d76956c7949300065b
Vinicius Almeida Nunes <vinunes.2004@gmail.com>
2023-05-10 20:59:28 -0300

    commit

--- Project1/src/main/java/com/Controller/Servlet.java
+++ Project1/src/main/java/com/Controller/Servlet.java
@@ -42,38 +42,6 @@ public class Servlet extends HttpServlet {
         // Obtém o valor do botão selecionado pelo usuário
         String button = request.getParameter("button");
         
-        // Verifica qual botão foi selecionado
-        if (button.equals("cadastrar")) {
-            
-            // Redireciona para a página de cadastro
-            response.sendRedirect("cadastroProduto.html");
-            
-        } else if (button.equals("excluir")) {
-            
-            // Redireciona para a página de exclusão
-            response.sendRedirect("excluirProduto.html");
-            
-        } else if (button.equals("alterar")) {
-            
-            // Redireciona para a página de alteração
-            response.sendRedirect("alterarProduto.html");
-            
-        
-        } else if (button.equals("listar")) {
-            
-            // Redireciona para a página de alteração
-            response.sendRedirect("listarProdutos.jsp");
-            
-            
-        }else if (button.equals("home")) {
-			response.sendRedirect("index.html");
-		}
-
-        else {
-            
-            // Redireciona para a página "homePage.html" novamente
-            response.sendRedirect("index.html");
-        }
 
     }
 
