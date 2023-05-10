2ed9e342148dc2a585dfc5aa16e78d318d609691
Thiago Yonekura <thiagoyonekura@gmail.com>
2023-05-09 22:07:47 -0300

    Adicionado conexão bd

--- Project1/src/main/java/com/Controller/Servlet.java
+++ Project1/src/main/java/com/Controller/Servlet.java
@@ -1,45 +1,40 @@
 package com.Controller;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.Models.*;
-import com.Repository.*;
+
+import com.Repository.BancoDados;
+
 
 
 @WebServlet(urlPatterns = { "/homePage", "/cadastroProdutos", "/listaProdutos", "/modificarProdutos" })
 public class Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	BancoDados r = new BancoDados("jdbc:mysql://localhost:3306/projeto1", "root", "");
-	
+	BancoDados bd = new BancoDados("jdbc:mysql://localhost:3306/projeto1", "root", "");
+    
+    public Servlet() {
+        super();
+        
+    }
+
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String method = request.getMethod();
-		if (method.equals("POST")) {
-			System.out.println("POST");
-			doPost(request, response);
-		} else if (method.equals("GET")) {
-			System.out.println("GET");
-			doGet(request, response);
-		}
+		
 	}
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String path = request.getServletPath();
-		if (path.equals("/homePage")) {
-			response.sendRedirect("home.html");
-		}
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		
+		doGet(request, response);
 	}
 
 }
